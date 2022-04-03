681beb22f6de212bf6490f890fdb050d564c74b3
NirmalMurugan <nirmalmurugan001@gmail.com>
2022-04-03 11:31:47 +0530

    AbiExplained

---
+++ src/org/abi/AbiGit.java
@@ -0,0 +1,16 @@
+package org.abi;
+
+public class AbiGit {
+	public static void main(String[] args) {
+
+		//To find the given 29 number is odd or even?
+		int n = 29;
+		
+		if (n % 2 == 0) {
+			System.out.println("Even");
+		} else {
+			System.out.println("Odd");
+		}
+
+	}
+}
